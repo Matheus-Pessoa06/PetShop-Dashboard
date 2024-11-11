a5999f12abc59ac97d368ba440b6eff8d5fba575
Matheus <matheuspessoa2010@gmail.com>
2024-11-10 23:03:57 -0300

    Adicionado validações e rotas

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email'
+        ];
+    }
+
+    public function messages(): array{
+        return [
+            'name.required' => 'Campo nome é obrigatorio',
+            'email.email' => "Email fornecido não é valido",
+            'password.min' => "Senha tem que conter pelo menos 8 caracteres",
+            'email.unique' => "Email já consta no banco de dados"
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+{
+        throw new HttpResponseException(response()->json([
+            'message' => 'Os dados fornecidos são inválidos.',
+            'errors' => $validator->errors()
+        ], 422));
+}
+}
