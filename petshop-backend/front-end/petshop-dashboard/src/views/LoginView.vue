a6ec86828582e8f6d3894656b6b0810cad6d637d
Matheus <matheuspessoa2010@gmail.com>
2025-05-15 21:34:33 -0300

    Inicio do front-end

---
+++ petshop-backend/front-end/petshop-dashboard/src/views/LoginView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="login-left">
+            <h1>Bem-Vindo!</h1>
+            <div class="formumalio-login">
+                <input type="email">
+                <input type="password">
+                <button>Registrar</button>
+            </div>
+        </div>
+
+        <div class="login-right">
+            <img src="../assets/fotos/950dcbf02121757abda6f9a89bb8bcf542737d40.jpg" alt="petshop">
+        </div>
+        
+    </div>
+</template>
+
+<style scoped>
+
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+.container{
+    display: flex;
+    align-items: center;
+}
+
+</style>
